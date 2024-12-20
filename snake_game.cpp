0b0f2f75eccc8cce52b339d425ab32c26b8676c4
ShreyaArora31 <arorashreya165@gmail.com>
2024-12-20 23:11:33 +0530

    pass through walls

--- snake_game.cpp
+++ snake_game.cpp
@@ -118,9 +118,18 @@ void Logic(){
         break;
     }
     
-    if(x>width || x<0 || y> height || y<0)
-        gameOver = true; //if we hit the wall
+    //if(x>width || x<0 || y> height || y<0)
+      //  gameOver = true; //if we hit the wall
     
+    if(x>=width)
+    x=0;
+    else if(x<0)
+    x=width-1;
+    if(y>=height)
+    y=0;
+    else if(y<0)
+    y=height-1;
+
     for(int i =0; i< Tail_len;i++){
         if(tailX[i] == x && tailY[i] == y)
         gameOver = true;
